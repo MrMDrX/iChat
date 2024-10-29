c4a60d31ad7ff6329df6c403ec7575562d904d8a
Med <geek.4tech007@gmail.com>
2024-10-29 11:06:19 +0000

    Added register page 💄

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:ichat/widgets/app_button.dart';
+import 'package:ichat/widgets/app_textfield.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  void signup() {
+    //TODO: Implement signup
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.message_rounded,
+              size: 76,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'iChat',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 32),
+            const Text(
+              'Let\'s create an account for you',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 32),
+            AppTextfield(
+              hintText: 'Email',
+              controller: emailController,
+              obscureText: false,
+            ),
+            const SizedBox(height: 16),
+            AppTextfield(
+              hintText: 'Password',
+              controller: passwordController,
+              obscureText: true,
+            ),
+            const SizedBox(height: 16),
+            AppTextfield(
+              hintText: 'Confirm Password',
+              controller: confirmPasswordController,
+              obscureText: true,
+            ),
+            const SizedBox(height: 24),
+            AppButton(text: 'Sign up', onTap: signup),
+            const SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Already a member ?"),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
